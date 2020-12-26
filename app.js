9c1157ac9d1c9b22322cbfb877e8859ce3f811bd
Shaurya Mathur <shaurya609>
2020-12-26 20:39:11 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#books").click(function(){
+        $("#bookdesc").slideToggle("slow");
+    });
+    $("#games").click(function(){
+        $("#gamedesc").slideToggle("slow");
+    });
+});
